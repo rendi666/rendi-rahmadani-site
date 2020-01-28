bf1be5b6c8243e3f4295111ac5b968aa9a13b33d
Rendi Rahmadani <rendi@detik.com>
2020-01-28 15:20:50 +0700

    Update css

--- src/templates/page.js
+++ src/templates/page.js
@@ -9,17 +9,17 @@ export default class Page extends React.Component {
         return (
             <Layout {...this.props}>
               <article className="post page post-full">
+                {_.get(this.props, 'pageContext.frontmatter.img_path') && 
+                <div className="post-thumbnail">
+                  <img className="thumbnail" src={safePrefix(_.get(this.props, 'pageContext.frontmatter.img_path'))} alt={_.get(this.props, 'pageContext.frontmatter.title')} />
+                </div>
+                }
                 <header className="post-header">
                   <h1 className="post-title">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
                 </header>
                 {_.get(this.props, 'pageContext.frontmatter.subtitle') && 
                 <div className="post-subtitle">
                   {htmlToReact(_.get(this.props, 'pageContext.frontmatter.subtitle'))}
-                </div>
-                }
-                {_.get(this.props, 'pageContext.frontmatter.img_path') && 
-                <div className="post-thumbnail">
-                  <img className="thumbnail" src={safePrefix(_.get(this.props, 'pageContext.frontmatter.img_path'))} alt={_.get(this.props, 'pageContext.frontmatter.title')} />
                 </div>
                 }
                 <div className="post-content">
